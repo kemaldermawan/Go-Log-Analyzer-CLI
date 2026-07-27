557b8e068329bcde378ed92b7ff92cf40eb796c8
kemaldermawan <kemaldermawan2707@gmail.com>
2026-07-27 08:25:23 +0700

    feat: implement security anomaly detection for brute force mitigation

--- analyzer/security.go
+++ analyzer/security.go
@@ -1,1 +1,16 @@
 package analyzer
+
+import "fmt"
+
+func DetectAnomalies(failedRequests map[string]int, threshold int) []string {
+	var alerts []string
+
+	for ip, count := range failedRequests {
+		if count >= threshold {
+			alert := fmt.Sprintf("[ALERT] IP %s flagged for Potential Brute Force / Scanning (%d Failed Attempts).", ip, count)
+			alerts = append(alerts, alert)
+		}
+	}
+
+	return alerts
+}
--- analyzer/stats.go
+++ analyzer/stats.go
@@ -13,17 +13,19 @@ type LogEntry struct {
 }
 
 type LogStats struct {
-	TotalLines  int            `json:"total_lines"`
-	StatusCodes map[string]int `json:"status_codes"`
-	IPRequests  map[string]int `json:"ip_requests"`
-	Endpoints   map[string]int `json:"endpoints"`
+	TotalLines     int            `json:"total_lines"`
+	StatusCodes    map[string]int `json:"status_codes"`
+	IPRequests     map[string]int `json:"ip_requests"`
+	Endpoints      map[string]int `json:"endpoints"`
+	FailedRequests map[string]int `json:"failed_requests"`
 }
 
 func NewLogStats() *LogStats {
 	return &LogStats{
-		StatusCodes: make(map[string]int),
-		IPRequests:  make(map[string]int),
-		Endpoints:   make(map[string]int),
+		StatusCodes:    make(map[string]int),
+		IPRequests:     make(map[string]int),
+		Endpoints:      make(map[string]int),
+		FailedRequests: make(map[string]int),
 	}
 }
 
@@ -32,6 +34,10 @@ func (s *LogStats) AddEntry(entry LogEntry) {
 	s.StatusCodes[entry.StatusCode]++
 	s.IPRequests[entry.IP]++
 	s.Endpoints[entry.Endpoint]++
+
+	if entry.StatusCode == "401" || entry.StatusCode == "404" {
+		s.FailedRequests[entry.IP]++
+	}
 }
 
 type kv struct {
@@ -44,11 +50,9 @@ func getTopN(m map[string]int, n int) []kv {
 	for k, v := range m {
 		ss = append(ss, kv{k, v})
 	}
-
 	sort.Slice(ss, func(i, j int) bool {
 		return ss[i].Value > ss[j].Value
 	})
-
 	if len(ss) > n {
 		return ss[:n]
 	}
@@ -75,6 +79,15 @@ func (s *LogStats) PrintReport(duration string) {
 	fmt.Println("\n[+] Top 5 Requested Endpoints:")
 	for _, kv := range getTopN(s.Endpoints, 5) {
 		fmt.Printf("    - %s : %d hits\n", kv.Key, kv.Value)
+	}
+	fmt.Println("\n[!] SECURITY ALERTS / ANOMALIES DETECTED:")
+	alerts := DetectAnomalies(s.FailedRequests, 3)
+	if len(alerts) == 0 {
+		fmt.Println("    [✓] No anomalies detected.")
+	} else {
+		for _, alert := range alerts {
+			fmt.Printf("    %s\n", alert)
+		}
 	}
 	fmt.Println("==================================================")
 }
