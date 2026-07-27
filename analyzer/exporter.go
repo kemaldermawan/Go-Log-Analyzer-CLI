b7938533f72a5b6ac34ea57cb6a9249ff4480f17
kemaldermawan <kemaldermawan2707@gmail.com>
2026-07-27 09:02:06 +0700

    feat: add CSV exporter and finalize comprehensive README documentation

--- analyzer/exporter.go
+++ analyzer/exporter.go
@@ -1,9 +1,11 @@
 package analyzer
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func (s *LogStats) ExportToJSON(outputPath string) error {
@@ -17,6 +19,43 @@ func (s *LogStats) ExportToJSON(outputPath string) error {
 		return fmt.Errorf("failed to write JSON file: %w", err)
 	}
 
-	fmt.Printf("[✓] Report successfully exported to: %s\n", outputPath)
+	fmt.Printf("[✓] JSON report successfully exported to: %s\n", outputPath)
+	return nil
+}
+
+func (s *LogStats) ExportToCSV(outputPath string) error {
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed to create CSV file: %w", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	header := []string{"Category", "Key", "Value"}
+	if err := writer.Write(header); err != nil {
+		return fmt.Errorf("failed to write CSV header: %w", err)
+	}
+
+	writer.Write([]string{"General", "Total Lines", strconv.Itoa(s.TotalLines)})
+
+	for k, v := range s.StatusCodes {
+		writer.Write([]string{"Status Code", k, strconv.Itoa(v)})
+	}
+
+	for k, v := range s.IPRequests {
+		writer.Write([]string{"IP Request", k, strconv.Itoa(v)})
+	}
+
+	for k, v := range s.Endpoints {
+		writer.Write([]string{"Endpoint", k, strconv.Itoa(v)})
+	}
+
+	for k, v := range s.FailedRequests {
+		writer.Write([]string{"Security Alert (Failed IP)", k, strconv.Itoa(v)})
+	}
+
+	fmt.Printf("[✓] CSV report successfully exported to: %s\n", outputPath)
 	return nil
 }
--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 func main() {
 	logFile := flag.String("file", "sample.log", "Path to the log file to analyze")
 	threshold := flag.Int("threshold", 3, "Failed request threshold for anomaly detection")
-	exportPath := flag.String("export", "", "Path to export the report as JSON (optional)")
+	jsonPath := flag.String("json", "", "Path to export the report as JSON (optional)")
+	csvPath := flag.String("csv", "", "Path to export the report as CSV (optional)")
 
 	flag.Parse()
-
 	start := time.Now()
 
 	stats, err := analyzer.ParseLogFile(*logFile)
@@ -24,13 +24,19 @@ func main() {
 	}
 
 	duration := time.Since(start).String()
-
 	stats.PrintReport(duration, *threshold)
 
-	if *exportPath != "" {
-		err := stats.ExportToJSON(*exportPath)
+	if *jsonPath != "" {
+		err := stats.ExportToJSON(*jsonPath)
 		if err != nil {
-			fmt.Printf("Error exporting report: %v\n", err)
+			fmt.Printf("Error exporting JSON report: %v\n", err)
+		}
+	}
+
+	if *csvPath != "" {
+		err := stats.ExportToCSV(*csvPath)
+		if err != nil {
+			fmt.Printf("Error exporting CSV report: %v\n", err)
 		}
 	}
 }
